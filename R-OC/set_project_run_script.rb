208e6b4033e400de243a0385be510f457bf5c366
peng jun <pengjun@yy.com>
2017-10-05 21:09:08 +0800

    Podspec update

--- R-OC/set_project_run_script.rb
+++ R-OC/set_project_run_script.rb
@@ -1,11 +1,24 @@
 #!/usr/bin/ruby
+ppp = "/Users/pengjun/Desktop/yy-svn/entmobile_ios_branches/entmobile-ios_6.6_LiveCommon_feature/aaaa.txt";
 
 require 'pathname'
 require 'xcodeproj'
 
-path_to_project = "${SOURCE_ROOT}/${PROJECT_NAME}.xcodeproj"
-project = Xcodeproj::Project.open(path_to_project)
-main_target = project.targets.first
-phase = main_target.new_shell_script_build_phase("R-OC Script")
-phase.shell_script = "ruby ${SRCROOT}/Pods/R-OC/R-OC/R-OC/BundleR.rb \"${SRCROOT}/${PROJECT_NAME}.xcodeproj\" \"${PROJECT_NAME}\""
-project.save()
+current_pwd="$PWD"
+project_dir=`cd "../../"; pwd`
+cd "$current_pwd"
+
+File.open(ppp,"w+").syswrite(current_pwd);#HOME + " 6 "+ PATH);
+
+# Get .xcodeproj file path (yes I know it's not a file)
+# project_file=`find "$project_dir" -maxdepth 1 -name "*.xcodeproj" | tail -1`
+#
+# SOURCE_ROOT = $*[0];
+# PROJECT_NAME = $*[1];
+#
+# path_to_project = SOURCE_ROOT+"/"+PROJECT_NAME+".xcodeproj"
+# project = Xcodeproj::Project.open(path_to_project)
+# main_target = project.targets.first
+# phase = main_target.new_shell_script_build_phase("R-OC Script")
+# phase.shell_script = "ruby ${SRCROOT}/Pods/R-OC/R-OC/R-OC/BundleR.rb \"${SRCROOT}/${PROJECT_NAME}.xcodeproj\" \"${PROJECT_NAME}\""
+# project.save()
