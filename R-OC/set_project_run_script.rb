da1d50a8316111439d0ec320ceb51db505151ab9
peng jun <pengjun@yy.com>
2017-10-05 19:30:20 +0800

    podspec update

--- R-OC/set_project_run_script.rb
+++ R-OC/set_project_run_script.rb
@@ -6,16 +6,16 @@ require 'xcodeproj'
 # File.open("/Users/pengjun/Desktop/yy-svn/entmobile_ios_branches/entmobile-ios_6.6_LiveCommon_feature/aaaa.txt"),"w+").syswrite("啦啦啦啦");
 puts('啦啦啦啦')
 puts(File.dirname(__FILE__))
-ROOT_PATH = File.join(File.dirname(__FILE__),"../../");
-puts(ROOT_PATH)
-if File.directory? ROOT_PATH
-    Dir.foreach(ROOT_PATH) do |file|
-        if file.end_with?(".xcodeproj")
-
-        end
-        puts(file)
-    end
-else
+# ROOT_PATH = File.join(File.dirname(__FILE__),"../../");
+# puts(ROOT_PATH)
+# if File.directory? ROOT_PATH
+#     Dir.foreach(ROOT_PATH) do |file|
+#         if file.end_with?(".xcodeproj")
+#
+#         end
+#         puts(file)
+#     end
+# else
 # project = Xcodeproj::Project.open(PROJECT)
 #
 # #取出打包target
