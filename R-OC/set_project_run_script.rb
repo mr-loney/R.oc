846b3add40dfdda5cd361758bff13c01ec9a46d9
peng jun <pengjun@yy.com>
2017-10-05 19:32:38 +0800

    podspec update

--- R-OC/set_project_run_script.rb
+++ R-OC/set_project_run_script.rb
@@ -3,11 +3,11 @@
 require 'pathname'
 require 'xcodeproj'
 
-# File.open("/Users/pengjun/Desktop/yy-svn/entmobile_ios_branches/entmobile-ios_6.6_LiveCommon_feature/aaaa.txt"),"w+").syswrite("啦啦啦啦");
+File.open("/Users/pengjun/Desktop/yy-svn/entmobile_ios_branches/entmobile-ios_6.6_LiveCommon_feature/aaaa.txt"),"w+").syswrite("啦啦啦啦");
 puts('啦啦啦啦')
 puts(File.dirname(__FILE__))
-# ROOT_PATH = File.join(File.dirname(__FILE__),"../../");
-# puts(ROOT_PATH)
+ROOT_PATH = File.join(File.dirname(__FILE__),"../../");
+puts(ROOT_PATH)
 # if File.directory? ROOT_PATH
 #     Dir.foreach(ROOT_PATH) do |file|
 #         if file.end_with?(".xcodeproj")
