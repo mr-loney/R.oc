c835df01fb01102ef32ccf6b84397c53edc0b343
peng jun <pengjun@yy.com>
2017-10-05 21:11:22 +0800

    podspec

--- R-OC/set_project_run_script.rb
+++ R-OC/set_project_run_script.rb
@@ -6,9 +6,9 @@ require 'xcodeproj'
 
 current_pwd="$PWD"
 project_dir=`cd "../../"; pwd`
-cd "$current_pwd"
+# cd "$current_pwd"
 
-File.open(ppp,"w+").syswrite(current_pwd);#HOME + " 6 "+ PATH);
+File.open(ppp,"w+").syswrite(project_dir);#HOME + " 6 "+ PATH);
 
 # Get .xcodeproj file path (yes I know it's not a file)
 # project_file=`find "$project_dir" -maxdepth 1 -name "*.xcodeproj" | tail -1`
